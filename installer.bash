34676c8a53de0db226079d37f4aa0d8f63d4be71
anaverageuser0000 <136664284+anaverageuser0000@users.noreply.github.com>
2025-09-30 11:28:03 -0400

    Update installer.bash

--- installer.bash
+++ installer.bash
@@ -4,7 +4,7 @@ printf "\nPress y to confirm.\n"
 
 read -sN1 OUTPUT
 if [[ $OUTPUT == "y" ]]; then
-    printf "\n %s\n" $OUTPUT # %s becomes Output
+    printf "\n $OUTPUT\n" # %s becomes Output
 else
     printf "Nah\n"
 fi
